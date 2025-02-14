17dc4c155228ba700fe33fef55a8c31c8943b63b
Preeti <karnwalpreeti102@gmail.com>
2025-02-14 18:01:04 +0530

    feat(encrypt.rs): Add byte substitution for aes encryption

--- src/aes_ecb/encrypt.rs
+++ src/aes_ecb/encrypt.rs
@@ -1,8 +1,24 @@
-use super::expand::expand_keys;
+use super::expand::{expand_keys, generate_match_table};
 use tfhe::prelude::*;
 use tfhe::{
     generate_keys, set_server_key, ConfigBuilder, FheUint8, MatchValues,
 };
+
+pub fn sub_bytes(state : &mut [FheUint8;16]){
+    let match_values = generate_match_table();
+    for byte in state.iter_mut(){
+        let (result, matched) = byte.match_values(&match_values).unwrap();
+        let matched = matched.decrypt(&client_key);
+        if matched{
+            *byte = result;
+        }
+    }
+}
+
+/// aes encrypt cipher block
+/// 1. generate round keys using key expansion
+/// 2. perform 9 rounds of sub_bytes, shift_rows, mix_columns and add_blocks
+/// 3. perform last round omitting mix_columns 
 
 pub fn encrypt(vi: &mut [FheUint8;16], key: &mut [FheUint8;16]){
     let mut state = *input;
